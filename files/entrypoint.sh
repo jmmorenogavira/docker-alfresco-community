adcf1a9c7bf59787910d53a7679ab7ee210d9cc7
Jose Manuel Moreno Gavira <josem.moreno.gavira@gmail.com>
2017-06-01 14:55:09 +0000

    execute apply_amps.sh on startup

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -9,6 +9,20 @@ echo "setting values for all the '-e ALF_xxx=...' parameters provided at startup
 bash /replacevars.sh;
 	
 bash /changepass.sh
+
+if [ -f /.apply_amps_created ]; 
+then
+    echo "apply_amps.sh done"
+else   
+	if grep -q ^read /opt/alfresco-community/bin/apply_amps.sh
+	then
+    	sed -i "/opt/alfresco-community/bin/apply_amps.sh" -e "s/read/#read/g"
+	fi
+	/opt/alfresco-community/bin/apply_amps.sh
+	touch /.apply_amps_created
+fi
+
+
 	
 echo "Starting Alfresco...";
 /opt/alfresco-community/alfresco.sh start;
